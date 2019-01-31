34cd647a19ffd18d1048a9da9cfcdf35509938fa
Olli Kortelahti <okortelahti@gmail.com>
2019-01-31 11:26:34 +0200

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import "./index.css";
 const canvas = document.getElementById("telegram-draw") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const dataURL = canvas.toDataURL();
-
 function mousePosition(event: any) {
   const canvasRect = canvas.getBoundingClientRect();
   const X = event.clientX - canvasRect.left;
   const Y = event.clientY - canvasRect.top;
-  draw(X, Y);
+  draw(X, Y, 5);
 }
 
-function draw(x: number, y: number) {
-  ctx.fillRect(x, y, 5, 5);
+function draw(x: number, y: number, weight: number) {
+  ctx.fillRect(x, y, weight, weight);
 }
-
-canvas.addEventListener("mousemove", mousePosition);

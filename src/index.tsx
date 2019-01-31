fa613f637f17779fbe6602d285d6c42784fd9b14
Olli Kortelahti <okortelahti@gmail.com>
2019-01-31 14:51:11 +0200

    draws better

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,31 @@ import "./index.css";
 const canvas = document.getElementById("telegram-draw") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-function mousePosition(event: any) {
-  const canvasRect = canvas.getBoundingClientRect();
-  const X = event.clientX - canvasRect.left;
-  const Y = event.clientY - canvasRect.top;
-  draw(X, Y, 5);
+const color = "black";
+const weight = 2;
+const pos = { x: 0, y: 0 };
+
+canvas.addEventListener("mousemove", e => {
+  draw(e);
+});
+canvas.addEventListener("mouseenter", setPosition);
+canvas.addEventListener("mousedown", setPosition);
+
+function setPosition(e: any) {
+  pos.x = e.clientX - canvas.offsetLeft;
+  pos.y = e.clientY - canvas.offsetTop;
 }
 
-function draw(x: number, y: number, weight: number) {
-  ctx.fillRect(x, y, weight, weight);
+function draw(e: any) {
+  if (e.buttons !== 1) {
+    return;
+  }
+
+  ctx.beginPath();
+  ctx.lineWidth = weight;
+  ctx.strokeStyle = color;
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+  ctx.stroke();
 }

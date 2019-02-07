56a4fdfd558bf42b0703d5a36f54a9d908229bcc
Olli Kortelahti <okortelahti@gmail.com>
2019-02-07 23:12:30 +0200

    add emoji size slider

--- src/menus.tsx
+++ src/menus.tsx
@@ -1,34 +1,45 @@
 import { settings } from "./inputs";
 
+const colorContainer = document.getElementById("colors") as HTMLDivElement;
+
+function addColorBtn(color: string) {
+  const newColor = document.createElement("button");
+  newColor.className = "colorbtn";
+  newColor.style.backgroundColor = color;
+  newColor.addEventListener("click", () => {
+    settings.selectedColor = color;
+    settings.mode = "draw";
+  });
+  colorContainer.insertAdjacentElement("afterbegin", newColor);
+}
 export function createColorMenu(colors: string[]) {
-  const colorContainer = document.getElementById("colors") as HTMLDivElement;
-  function addColorBtn(color: string) {
-    const newColor = document.createElement("button");
-    newColor.className = "colorbtn";
-    newColor.style.backgroundColor = color;
-    newColor.addEventListener("click", () => {
-      settings.selectedColor = color;
-      settings.mode = "draw";
-    });
-    colorContainer.insertAdjacentElement("afterbegin", newColor);
-  }
   colors.forEach(addColorBtn);
 }
 
+const emojiMenuContainer = document.getElementById("emojis") as HTMLDivElement;
 export function createEmojiMenu(emojis: string[]) {
-  const emojiMenuContainer = document.getElementById(
-    "emojis"
-  ) as HTMLDivElement;
-
-  function addEmojiButton(emoji: any) {
-    const newEmoji = document.createElement("div");
-    newEmoji.className = "emojibtn";
-    newEmoji.innerHTML = emoji;
-    newEmoji.addEventListener("click", () => {
-      settings.selectedEmoji = emoji;
-      settings.mode = "emoji";
-    });
-    emojiMenuContainer.insertAdjacentElement("beforeend", newEmoji);
-  }
   emojis.forEach(addEmojiButton);
+  emojiMenuContainer.insertAdjacentElement("afterend", emojiSizeSlider());
+}
+function addEmojiButton(emoji: any) {
+  const newEmoji = document.createElement("div");
+  newEmoji.className = "emojibtn";
+  newEmoji.innerHTML = emoji;
+  newEmoji.addEventListener("click", () => {
+    settings.selectedEmoji = emoji;
+    settings.mode = "emoji";
+  });
+  emojiMenuContainer.insertAdjacentElement("beforeend", newEmoji);
+}
+
+function emojiSizeSlider() {
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = "20";
+  slider.max = "150";
+  slider.id = "emojislider";
+  slider.addEventListener("input", () => {
+    settings.emojiSize = Number(slider.value);
+  });
+  return slider;
 }
